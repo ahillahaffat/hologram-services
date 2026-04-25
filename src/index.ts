c18fb99fa746d605f145b51b5d8c53648a15e39b
ahillahaffat <kammala88@gmail.com>
2026-04-25 13:12:27 +0700

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,15 @@ import 'dotenv/config'
 import { Hono } from 'hono'
 import { serve } from '@hono/node-server'
 import chatRouter from './routes/chat.route.js';
+import { cors } from 'hono/cors';
 
 const app = new Hono()
+app.use("*", cors());
 app.route("/", chatRouter);
 
 app.get('/', (c) => c.text('Hologram API running 🚀'))
 
-const port = Number(process.env.PORT) || 3000
+const port = Number(process.env.PORT) || 3001
 
 serve({
   fetch: app.fetch,
